70848517ec0de2c54a108d4931f975d1e4780e83
Farok Hossain <farokafs7@gmail.com>
2023-11-23 09:14:02 +0600

    toast component added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import "./globals.css";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 
+import { Toaster } from "@/components/ui/toaster";
+
 const App = () => {
   return (
     <main className="flex h-screen">
@@ -22,6 +24,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster />
     </main>
   );
 };
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
+import { useToast } from "@/components/ui/use-toast";
 
 import {
   Form,
@@ -18,6 +19,7 @@ import { Link } from "react-router-dom";
 import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const { toast } = useToast();
   const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -33,7 +35,11 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
-    console.log(newUser);
+    if (!newUser) {
+      return toast({
+        title: "Sign up failed. Please try again.",
+      });
+    }
   }
   return (
     <Form {...form}>
