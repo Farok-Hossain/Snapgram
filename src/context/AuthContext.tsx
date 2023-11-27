41a8bfad834cdd5af90eda6d7758a7c96fdb2bfb
Farok Hossain <farokafs7@gmail.com>
2023-11-27 09:38:35 +0600

    authenticated added

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,6 @@
-import { IContextType } from "@/types";
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IContextType, IUser } from "@/types";
+import { useState } from "react";
 
 export const INITIAL_USER = {
   id: "",
@@ -20,8 +22,39 @@ const INITIAL_SATE = {
 
 const AuthContext = createContext<IContextType>(INITIAL_SATE);
 
-const AuthContext = () => {
-  return <div></div>;
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthUser = async () = {
+    try {
+      const {$id, name, username, email, imageUrl, bio} = await getCurrentUser();
+      if($id) {
+        setUser({id: $id, name, username, email, imageUrl, bio })
+      }
+    } catch (error) {
+      console.log(error)
+      return false;
+    } finally{
+      setIsLoading(false);
+    }
+  };
+
+  const value = {
+    user, 
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,7 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
 import { account, appwriteConfig, avatars, databases } from "./config";
+import { Query } from "@tanstack/react-query";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -54,6 +55,24 @@ export async function signInAccount(user: { email: string; password: string }) {
   try {
     const session = await account.createEmailSession(user.email, user.password);
     return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
   } catch (error) {
     console.log(error);
   }
