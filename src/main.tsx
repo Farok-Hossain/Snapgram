c29f8188116627cacbaceb2d75087ce3bbb6ec87
Farok Hossain <farokafs7@gmail.com>
2023-12-11 11:19:49 +0600

    queryProvider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
